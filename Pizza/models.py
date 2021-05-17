0b0e268ba6504c56840703dced4effb90ca82a94
André Luis Moreira <andreluismoreirasmo@gmail.com>
2021-05-17 19:39:19 -0300

    Uplodad de imagens

--- Pizza/models.py
+++ Pizza/models.py
@@ -4,10 +4,9 @@ from django.db import models
 class Pizza(models.Model):
     name = models.CharField(max_length=100, unique=True, help_text='Nome')
     value = models.FloatField(null=False, help_text='Valor')
-    length = models.CharField(
-        max_length=1, default='P', help_text='P - Pequena, M - Média, G - Grande')
     detail = models.CharField(
         max_length=500, help_text='Sabor ou qualquer outra informação.', null=True)
+    photo = models.ImageField(upload_to='media', default='default.jpg')
 
     def __str__(self):
         return self.name
--- meuProjeto/urls.py
+++ meuProjeto/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home, areaRestrita
 
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('', home),
     path('areaRestrita/', areaRestrita),
     path('admin/', admin.site.urls),    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
